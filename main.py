2aa69fdffc8a5380a091a8be5c26a9207b7a9ac9
Igor Moreira Lopes <igor.moreira.lopes22@gmail.com>
2025-11-20 15:42:24 -0300

    Second commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Main file, where the menu will be shown
+
+while True:
+    print("1. Insert Line")
+    print("2. Remove Line")
+    print("3. Edit Line")
+    print("4. Show Schedule for a city")
+    print("5. Show available seats for a bus")
+    print("\n")
+
+    op = input("Insert a option")
+
+    match op:
+        case '1':
+            pass
+
+        case '2:
